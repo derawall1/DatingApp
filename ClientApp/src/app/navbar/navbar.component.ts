f2e8a8c471f39e6aac36d808a9933abe5892bb1a
Muhammad Mushtaq <50014170+derawall1@users.noreply.github.com>
2021-02-28 22:39:08 +0500

    end of section 6

--- ClientApp/src/app/navbar/navbar.component.ts
+++ ClientApp/src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 //import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -12,23 +14,31 @@ export class NavbarComponent implements OnInit {
   model:any = {};
  // currentUser$:Observable<User>;
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, 
+              private router: Router,
+              private toastr: ToastrService
+              
+              ) { }
 
   ngOnInit(): void {
     //this.currentUser$=this.accountService.currentUser$;
   }
   login(){
     this.accountService.login(this.model)
-          .subscribe(response =>{
-            console.log(response);
+          .subscribe(res =>{
+            this.router.navigateByUrl('/members');
             
-          }, error =>{
-            console.log(error);
+          }, err =>{
+            this.toastr.error(err.error);
+            
           })
   }
   logout(){
+    this.model.username='';
+    this.model.password='';
    
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
   
--- ClientApp/src/app/register/register.component.ts
+++ ClientApp/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 
@@ -11,7 +12,9 @@ export class RegisterComponent implements OnInit {
  // @Input() users: any;
   @Output() cancelRegister =new EventEmitter();
   model: any = {};
-  constructor(private accountService:AccountService) { }
+  constructor(private accountService:AccountService,
+              private toastr: ToastrService
+              ) { }
 
   ngOnInit(): void {
   }
@@ -21,7 +24,7 @@ export class RegisterComponent implements OnInit {
         console.log(res);
         this.cancel();
       }, err =>{
-        console.log(err);
+        this.toastr.error(err.error);
       })
   }
   cancel() {
